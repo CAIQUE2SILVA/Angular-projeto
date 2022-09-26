2b1c14b305be793bba5afe916d3be9e9e9eaef62
CAIQUE2SILVA <caique2silva@gmail.com>
2022-09-26 20:02:11 -0300

    intalando modulos

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Hero{
+
+//   constructor(public ataque : number) {
+
+//   }
+
+//   atacar() {
+//     //ataca
+//   }
+//   defender() {
+//     //defende
+//   }
+// }
+
+// const myHero = new Hero (1);
+
+// const otherHero = new Hero (2);
+
