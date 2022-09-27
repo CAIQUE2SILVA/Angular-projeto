f3d722144933aa84ae52f689dd6de830a7a006f1
CAIQUE2SILVA <caique2silva@gmail.com>
2022-09-27 19:18:31 -0300

    aula 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListPageComponent } from './list-page/list-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListPageComponent
+
   ],
   imports: [
     BrowserModule,
